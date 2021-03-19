c99e013148345ce6045223a21f471f7956e33813
Stan Bankras <stanbankras@gmail.com>
2021-03-19 13:52:12 +0100

    Delete album

--- src/routes/albums/index.js
+++ src/routes/albums/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import { isMongoId } from '../../modules/utils';
 import Album from '../../schemas/Album';
@@ -18,6 +19,20 @@ router.get('/', async (req, res) => {
   } else {
     res.redirect('/');
   }
+});
+
+router.get('/:id', async (req, res) => {
+  const album = await Album.findById(req.params.id);
+  res.render('albums/album', { pageTitle: album.name, album, userId: req.query.userId });
+});
+
+router.post('/:id/delete', async (req, res) => {
+  const album = await Album.findByIdAndDelete(req.params.id);
+  const user = await User.findOne({ albums: mongoose.Types.ObjectId(req.params.id) });
+  user.albums = user.albums.filter(a => a.toString() !== req.params.id);
+  await user.save();
+
+  res.redirect(`/albums?id=${req.query.userId}`)
 });
 
 export default router;
