78049ce76d74595ab5253aedcaf5678e815461ed
Stan Bankras <stanbankras@gmail.com>
2021-03-23 15:15:51 +0100

    Fix build process

--- src/modules/multer.js
+++ src/modules/multer.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '..', '..', 'src', 'data', 'images'))
+    cb(null, path.join(__dirname, '..', 'data', 'images'))
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)) //Appending .jpg
