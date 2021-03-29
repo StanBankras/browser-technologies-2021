2d8b8ceb200bc6f6e8ed7f3f87b2783e1be4245b
Stan Bankras <stanbankras@gmail.com>
2021-03-29 19:58:52 +0200

    Change slideshow code into iife

--- src/public/js/carousel.js
+++ src/public/js/carousel.js
@@ -1,61 +1,64 @@
-const carousel = document.getElementById('carousel');
-const container = document.querySelector('.container');
-const photos = document.querySelector('.photos');
-const nextButton = document.getElementById('next');
-const prevButton = document.getElementById('prev');
-const playButton = document.getElementById('play');
-const containerWidth = container.getBoundingClientRect().width;
-const amountOfPhotos = carousel.dataset.photos;
-let index = 0;
-let direction = 'next';
-let auto = false;
-
-// Add classes and HTML that is needed when Javascript works
-photos.classList.add('js');
-carousel.insertAdjacentHTML('beforeEnd', `
-  <div class="controls">
-    <button id="prev">Previous</button>
-    <button id="play">Play slideshow</button>
-    <button id="next">Next</button>
-  </div>
-`);
-
-
-nextButton.addEventListener('click', () => {
-  if(index < amountOfPhotos - 1) index++;
-  auto = false;
-  playButton.innerText = 'Play slideshow'
-  photos.style.transform = `translate(${-index * 316}px, 0)`;
-});
-
-prevButton.addEventListener('click', () => {
-  if(index > 0) index--;
-  auto = false;
-  playButton.innerText = 'Play slideshow'
-  photos.style.transform = `translate(${-index * 316}px, 0)`;
-});
-
-playButton.addEventListener('click', () => {
-  if(auto) {
+(function() {
+  const carousel = document.getElementById('carousel');
+  const container = document.querySelector('.container');
+  const photos = document.querySelector('.photos');
+  
+  // Add classes and HTML that is needed when Javascript works
+  photos.classList.add('js');
+  carousel.insertAdjacentHTML('beforeEnd', `
+    <div class="controls">
+      <button id="prev">Previous</button>
+      <button id="play">Play slideshow</button>
+      <button id="next">Next</button>
+    </div>
+  `);
+  
+  const nextButton = document.getElementById('next');
+  const prevButton = document.getElementById('prev');
+  const playButton = document.getElementById('play');
+  const containerWidth = container.getBoundingClientRect().width;
+  const amountOfPhotos = carousel.dataset.photos;
+  let index = 0;
+  let direction = 'next';
+  let auto = false;
+  
+  
+  nextButton.addEventListener('click', () => {
+    if(index < amountOfPhotos - 1) index++;
     auto = false;
     playButton.innerText = 'Play slideshow'
-  } else {
-    playButton.innerText = 'Stop slideshow'
-    auto = true;
-    autoSlideShow();
-  }
-});
-
-function autoSlideShow() {
-  if(!auto) return;
-  if(index < amountOfPhotos - 1 && direction === 'next') index++;
-  if(index > 0 && direction === 'prev') index--;
-  if(index === amountOfPhotos - 1) {
-    direction = 'prev';
-  }
-  if(index === 0) {
-    direction = 'next';
-  }
-  photos.style.transform = `translate(${-index * 316}px, 0)`;
-  setTimeout(autoSlideShow, 2000);
-}
+    photos.style.transform = `translate(${-index * 316}px, 0)`;
+  });
+  
+  prevButton.addEventListener('click', () => {
+    if(index > 0) index--;
+    auto = false;
+    playButton.innerText = 'Play slideshow'
+    photos.style.transform = `translate(${-index * 316}px, 0)`;
+  });
+  
+  playButton.addEventListener('click', () => {
+    if(auto) {
+      auto = false;
+      playButton.innerText = 'Play slideshow'
+    } else {
+      playButton.innerText = 'Stop slideshow'
+      auto = true;
+      autoSlideShow();
+    }
+  });
+  
+  function autoSlideShow() {
+    if(!auto) return;
+    if(index < amountOfPhotos - 1 && direction === 'next') index++;
+    if(index > 0 && direction === 'prev') index--;
+    if(index === amountOfPhotos - 1) {
+      direction = 'prev';
+    }
+    if(index === 0) {
+      direction = 'next';
+    }
+    photos.style.transform = `translate(${-index * 316}px, 0)`;
+    setTimeout(autoSlideShow, 2000);
+  } 
+})();
